781a95cacacb54a60d648dac88e38c4b2822dd8b
Lee9747 <leejunho32@naver.com>
2025-05-01 20:26:49 +0900

    return; 없애고  CSkillObject::Update();하도록

--- Project/Engine/FireSword.cpp
+++ Project/Engine/FireSword.cpp
@@ -40,18 +40,19 @@ void CFireSword::Update()
     {
         Vec3 pos = GetTransform()->GetRelativePosition();
 
+        float t = 0.f;
+
         if (!m_ReadyToRotate)
         {
             m_Elapsed += DELTA_TIME;
-            float t = m_Elapsed / m_TranslateWaitTime;
-            t = std::clamp(t, 0.0f, 1.0f);
+            t = std::clamp(m_Elapsed / m_TranslateWaitTime, 0.0f, 1.0f);
 
             for (auto obj : GetChild())
             {
                 obj->GetTransform()->SetRelativeScale(t, t, t);
             }
 
-            float readySpeed = m_Speed * ((1.0f - t) / 1.0f);
+            float readySpeed = m_Speed * (1.0f - t);
             pos += m_ReadyDirection * readySpeed * DELTA_TIME;
             GetTransform()->SetRelativePosition(pos);
 
@@ -59,21 +60,18 @@ void CFireSword::Update()
             {
                 m_ReadyToRotate = true;
                 m_Elapsed = 0.f;
+
                 Vec3 targetDir = m_TargetPos - pos;
                 targetDir.Normalize();
                 m_Direction = targetDir;
-
             }
-            return;
         }
 
         if (m_ReadyToRotate && !m_ReadyToFire)
         {
             m_Elapsed += DELTA_TIME;
-            float t = m_Elapsed / m_RotateWaitTime;
-            t = std::clamp(t, 0.0f, 1.0f);
+            t = std::clamp(m_Elapsed / m_RotateWaitTime, 0.0f, 1.0f);
 
-            // 보간된 방향o
             Vec3 blendedDirection = DirectX::SimpleMath::Vector3::Lerp(m_ReadyDirection, m_Direction, t);
             blendedDirection.Normalize();
             GetTransform()->LookAt(blendedDirection);
@@ -82,15 +80,18 @@ void CFireSword::Update()
             {
                 m_ReadyToFire = true;
                 m_Elapsed = 0.f;
-                m_FireParticleId = CParticleSystemManager::GetInst()->AddEmitter(L"Spark", GetTransform()->GetRelativePosition());
 
+                m_FireParticleId = CParticleSystemManager::GetInst()->AddEmitter(L"Spark", GetTransform()->GetRelativePosition());
             }
-            return;
         }
 
-        pos += m_Direction * m_Speed * DELTA_TIME;
-        GetTransform()->SetRelativePosition(pos);
+        if (m_ReadyToFire)
+        {
+            pos += m_Direction * m_Speed * DELTA_TIME;
+            GetTransform()->SetRelativePosition(pos);
+        }
     }
+
     CSkillObject::Update();
 }
 
